c7f4b8c7650304e1a0e68df4c63eb5224deb8962
Shihan Ai <shihan.ai@mail.utoronto.ca>
2015-03-02 04:03:57 -0500

    wow

--- server.js
+++ server.js
@@ -1,46 +1,33 @@
 var mysql = require('mysql');
 var express = require('express');
-var fs = require('fs');
+var bodyParser = require('body-parser');
+var user = require('./src/js/user');
 var app = express();
 
-var connection = mysql.createConnection({
-	host: 'Shihan',
-	user: 'g3aishih',
-	password: 'g3aishih!password',
-	database: 'communityfund',
-	port: 3306
-});
+//For parsing body
+app.use(bodyParser.urlencoded({ extended: false}));
 
-connection.connect();
-
-
-var s = connection.query('SELECT * FROM user', function(err, result, fields){
-	if (err) {
-		throw err;
-	} else {
-		console.log('result: ', result);
-	}
-});
-
-connection.end();
-
-
+//Root page
 app.get('/', function(req, res) {
 	res.sendFile('src/html/root.html', {root: __dirname});
 });
 
+//Login page
 app.get('/login.html', function(req, res) {
 	res.sendFile('src/html/login.html', {root: __dirname});
 });
 
+//Sign up page
 app.get('/signup.html', function(req, res) {
 	res.sendFile('src/html/signup.html', {root: __dirname});
 });
 
+//Set up page
 app.get('/setup.html', function(req, res) {
 	res.sendFile('src/html/setup.html', {root: __dirname});
 });
 
+//Lost password page
 app.get('/lostpw.html', function(req, res) {
 	res.sendFile('src/html/lostpw.html', {root: __dirname});
 });
@@ -50,7 +37,18 @@ app.post('/signup', function(req, res) {
 	var email = req.body.email;
 	var password = req.body.password;
 	var repassword = req.body.repassword;
-	
+
+	//Authenticate signup
+	user.authenticateSignUp(email, password, repassword, function(success) {
+		if (success) {
+			user.createUser(name, email, password);
+			res.sendFile('src/html/login.html', {root: __dirname});
+		} else {
+			//Authentication failed
+			console.log("Authentication failed");
+			res.sendFile('src/html/signup.html', {root: __dirname});
+		}
+	});
 });
 
 app.listen(8080);
